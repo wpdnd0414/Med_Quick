7d9d5c18282b33362b5b69013cc0ebbbe4520bae
kod0406 <kod04061@gmail.com>
2024-05-13 21:15:46 +0900

    Upload_Activitys
    액티비티와 xml 업로드

---
+++ app/src/main/java/com/example/quick_med/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.quick_med
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonAlarm = findViewById<Button>(R.id.button_alarm)
+        buttonAlarm.setOnClickListener {
+            val intent = Intent(this, Alarm::class.java)
+            startActivity(intent)
+        }
+
+        val buttonCalendar = findViewById<Button>(R.id.button_calendar)
+        buttonCalendar.setOnClickListener {
+            val intent = Intent(this, Calendar::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMyMed = findViewById<Button>(R.id.button_my_med)
+        buttonMyMed.setOnClickListener {
+            val intent = Intent(this, My_Med::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSearchMed = findViewById<Button>(R.id.button_search_med)
+        buttonSearchMed.setOnClickListener {
+            val intent = Intent(this, SearchMed::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quick_med/My_Med.kt
@@ -0,0 +1,20 @@
+package com.example.quick_med
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class My_Med : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_med)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
