7ce23d8ce0466d9a8a3b9068e7b3b56f846b1b99
kod0406 <kod04061@gmail.com>
2024-05-17 14:40:19 +0900

    Update
    인트로 기능 초기 생성

---
+++ app/src/main/java/com/example/quick_med/Intro.kt
@@ -0,0 +1,29 @@
+package com.example.quick_med
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quick_med.ui.theme.Quick_MedTheme
+import android.os.Handler
+import android.content.Intent
+
+class Intro : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, Intro2::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/example/quick_med/Intro2.kt
@@ -0,0 +1,34 @@
+package com.example.quick_med
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quick_med.ui.theme.Quick_MedTheme
+import android.os.Handler
+import android.content.Intent
+
+class Intro2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro2)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+        // Do nothing
+    }
+}
