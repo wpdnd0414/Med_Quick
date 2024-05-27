ed7911d661fb0bb8a9d6e531701db8f360730deb
kod0406 <kod04061@gmail.com>
2024-05-27 14:46:56 +0900

    Update_Result
    머지 잘못해서 날라갈뻔한거 수정

--- app/src/main/java/com/example/quick_med/Intro.kt
+++ app/src/main/java/com/example/quick_med/Intro.kt
@@ -12,17 +12,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.os.Handler
 import android.content.Intent
+import android.os.Looper
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
-class Intro : ComponentActivity() {
+class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        var handler = Handler()
-        handler.postDelayed({
+        // Ensure the ImageView is being set properly
+        val introImageView = findViewById<ImageView>(R.id.introImageView)
+
+        // 3초 후에  액티비티로 전환
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, Intro2::class.java)
             startActivity(intent)
-            finish()
-        }, 3000)
+            finish() // 인트로 액티비티를 종료하여 뒤로 가기 버튼을 눌렀을 때 다시 돌아오지 않게 함
+        }, 3000) // 3000밀리초 == 3초
     }
 }
