7f2dd70d2feb82f2d8b5ddd2c119de63904703a4
kod0406 <kod04061@gmail.com>
2024-05-27 12:41:43 +0900

    Update API_Search
    이미지 잘나옴 헤헤헤

--- app/src/main/java/com/example/quick_med/Intro.kt
+++ app/src/main/java/com/example/quick_med/Intro.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.quick_med.ui.theme.Quick_MedTheme
 import android.os.Handler
 import android.content.Intent
 import android.os.Looper
