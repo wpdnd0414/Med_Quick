29b1267256f5b68d3845dafd817517a88b83207d
Alea14 <jeawoong414@naver.com>
2024-06-02 17:22:42 +0900

    Revert "Merge branch 'Sub_02'"
    This reverts commit 1858fbcd12833424d4bdfb0e0af2764453a9fbea, reversing
    changes made to 419b2484ac67e1360adc69c928f35e7d79357191.

--- app/src/main/java/com/example/quick_med/AlarmData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quick_med
-
-data class AlarmData(
-    val name: String,
-    val hour: Int,
-    val minute: Int,
-    val daysOfWeek: BooleanArray,
-    var isEnabled: Boolean // 알람 활성화 상태를 저장하는 변수
-)
